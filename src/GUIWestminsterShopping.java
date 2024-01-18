ef839b59a5249a9d841f38364e8f46ecaffdc21b
Nishadi Perera <124398259+nishadi220@users.noreply.github.com>
2024-01-18 20:21:18 +0530

    1/18/2024 - 8.421pm - Made changes to GUI - Product details panel

--- src/GUIWestminsterShopping.java
+++ src/GUIWestminsterShopping.java
@@ -5,7 +5,6 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class GUIWestminsterShopping {
     static String selectedCategory;
@@ -15,8 +14,51 @@ public class GUIWestminsterShopping {
     static Object price;
     static Object information;
 
-    public static void main(String[] args) {
+    static public void ProductDetails(Object productId, JTable productsTable){
+        for (Product item : WestminsterShoppingManager.list_of_products){
+            if (productId == item.getProduct_ID()){
+                if (item instanceof Electronics){
+                    JPanel bottomPanel = new JPanel(new GridLayout(0, 1));
+                    JLabel SelectedProductLabel = new JLabel("Select Product - Details ");
+                    JLabel ProductIDLabel = new JLabel("Product ID : " + item.getProduct_ID());
+                    JLabel CategoryLabel = new JLabel("Category : Electronics");
+                    JLabel NameLabel = new JLabel("Name : " + item.getProduct_name());
+                    JLabel BrandLabel = new JLabel("Brand : " + ((Electronics) item).getProduct_brand());
+                    JLabel WarrantyLabel = new JLabel("Warranty : " + ((Electronics) item).getProduct_warranty());
+                    JLabel ItemsAvailableLabel = new JLabel("Items Available : " + item.getNo_available_products());
 
+                    bottomPanel.add(SelectedProductLabel);
+                    bottomPanel.add(ProductIDLabel);
+                    bottomPanel.add(CategoryLabel);
+                    bottomPanel.add(NameLabel);
+                    bottomPanel.add(BrandLabel);
+                    bottomPanel.add(WarrantyLabel);
+                    bottomPanel.add(ItemsAvailableLabel);
+
+                } else if (item instanceof Clothing) {
+                    JPanel bottomPanel = new JPanel(new GridLayout(0, 1));
+                    JLabel SelectedProductLabel = new JLabel("Select Product - Details ");
+                    JLabel ProductIDLabel = new JLabel("Product ID : " + item.getProduct_ID());
+                    JLabel CategoryLabel = new JLabel("Category : Clothing ");
+                    JLabel NameLabel = new JLabel("Name : " + item.getProduct_name());
+                    JLabel SizeLabel = new JLabel("Size : " + ((Clothing) item).getProduct_size());
+                    JLabel ColourLabel = new JLabel("Colour : " + ((Clothing) item).getProduct_color());
+                    JLabel ItemsAvailableLabel = new JLabel("Items Available : " + item.getNo_available_products());
+
+                    bottomPanel.add(SelectedProductLabel);
+                    bottomPanel.add(ProductIDLabel);
+                    bottomPanel.add(CategoryLabel);
+                    bottomPanel.add(NameLabel);
+                    bottomPanel.add(SizeLabel);
+                    bottomPanel.add(ColourLabel);
+                    bottomPanel.add(ItemsAvailableLabel);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
         WestminsterShoppingManager.load_products();
 
         JFrame frame = new JFrame("Westminster Online Shopping System");
@@ -24,7 +66,6 @@ public class GUIWestminsterShopping {
         JComboBox<String> categoryComboBox = new JComboBox<>(new String[]{"All", "Electronics", "Clothes"});
         JButton shoppingCartButton = new JButton("Shopping Cart");
         JButton addToCartButton = new JButton("Add to Shopping Cart");
-        JTextArea itemDetailsTextArea = new JTextArea();
 
         String[] columnNames = {"Product ID", "Name", "Category", "Price(£)", "Information"};
 
@@ -38,14 +79,9 @@ public class GUIWestminsterShopping {
         }
 
         JScrollPane tableScrollPane = new JScrollPane(productsTable);
-        tableScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // Creating a scroll pane for the table
-
-        // Creating the table
-        ArrayList<Object[]> tableData = new ArrayList<>();
+        tableScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         // Set layout
-
-        // Top panel with category label, dropdown, and shopping cart button
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JLabel categoryLabel = new JLabel("Select Product Category");
         topPanel.add(categoryLabel);
@@ -55,77 +91,54 @@ public class GUIWestminsterShopping {
         shoppingCartPanel.add(shoppingCartButton);
         topPanel.add(shoppingCartPanel);
         topPanel.add(tableScrollPane, BorderLayout.CENTER);
+        topPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 10, 50));
         topPanel.setPreferredSize(new Dimension(800, 400));
 
         productsTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 if (!e.getValueIsAdjusting()) {
-                    // User has selected a cell
                     int selectedRow = productsTable.getSelectedRow();
                     if (selectedRow != -1) {
-                        // Get the product information based on the selected row
-                        productId = productsTable.getValueAt(selectedRow, 0);
-                        productName = productsTable.getValueAt(selectedRow, 1);
-                        category = productsTable.getValueAt(selectedRow, 2);
-                        price = productsTable.getValueAt(selectedRow, 3);
-                        information = productsTable.getValueAt(selectedRow, 4);
+                        Object productId = productsTable.getValueAt(selectedRow, 0);
+                        GUIWestminsterShopping.ProductDetails(productId, productsTable);
                     }
                 }
             }
         });
 
-        JPanel bottomPanel = new JPanel(new GridLayout(0,1));
-        JLabel SelectedProductLabel = new JLabel("Select Product - Details ");
-        JLabel ProductIDLabel = new JLabel("Product ID : " + productId);
-        JLabel CategoryLabel = new JLabel("Category : " + category);
-        JLabel NameLabel = new JLabel("Name : " + productName);
-        JLabel SizeLabel = new JLabel("Size : " + information);
-        JLabel ColourLabel = new JLabel("Colour : " + information);
-        JLabel ItemsAvailableLabel = new JLabel("Items Available : " + information);
-
-        bottomPanel.add(SelectedProductLabel);
-        bottomPanel.add(ProductIDLabel);
-        bottomPanel.add(CategoryLabel);
-        bottomPanel.add(NameLabel);
-        bottomPanel.add(SizeLabel);
-        bottomPanel.add(ColourLabel);
-        bottomPanel.add(ItemsAvailableLabel);
-
         JPanel addToCartPanel = new JPanel(new BorderLayout());
         addToCartPanel.add(addToCartButton, BorderLayout.SOUTH);
+
+        JPanel bottomPanel = new JPanel(new GridLayout(0, 1));
+
         bottomPanel.add(addToCartPanel);
-        bottomPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 10, 50));
-        bottomPanel.setPreferredSize(new Dimension(800, 400));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 10, 50));
+        bottomPanel.setPreferredSize(new Dimension(800, 300));
 
         frame.add(topPanel, BorderLayout.NORTH);
         frame.add(bottomPanel, BorderLayout.SOUTH);
 
-        categoryComboBox.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
+        categoryComboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 selectedCategory = (String) categoryComboBox.getSelectedItem();
-
-                // Clear the existing data in the table
                 tableModel.setRowCount(0);
 
-                // Populate the tableData list based on the selected category
-                for (Product product : WestminsterShoppingManager.list_of_products){
-
+                for (Product product : WestminsterShoppingManager.list_of_products) {
                     String Category = "";
                     String Information = "";
                     Object[] ProductArray;
 
-                    if (selectedCategory.equals("All")){
-                        if (product instanceof Electronics){
+                    if (selectedCategory.equals("All")) {
+                        if (product instanceof Electronics) {
                             Category = "Electronics";
                             Information = ((Electronics) product).getProduct_brand() + "," + ((Electronics) product).getProduct_warranty();
-                        }
-                        else if (product instanceof Clothing){
+                        } else if (product instanceof Clothing) {
                             Category = "Clothes";
                             Information = ((Clothing) product).getProduct_color() + "," + ((Clothing) product).getProduct_size();
                         }
 
-                        ProductArray = new Object []{
+                        ProductArray = new Object[]{
                                 product.getProduct_ID(),
                                 product.getProduct_name(),
                                 Category,
@@ -134,17 +147,15 @@ public class GUIWestminsterShopping {
                         };
 
                         tableModel.addRow(ProductArray);
-
-                    }
-                    else if (selectedCategory.equals("Electronics")){
+                    } else if (selectedCategory.equals("Electronics")) {
                         if (!(product instanceof Electronics)) {
-                                continue;
+                            continue;
                         }
 
                         Category = "Electronics";
                         Information = ((Electronics) product).getProduct_brand() + "," + ((Electronics) product).getProduct_warranty();
 
-                        ProductArray = new Object []{
+                        ProductArray = new Object[]{
                                 product.getProduct_ID(),
                                 product.getProduct_name(),
                                 Category,
@@ -153,9 +164,7 @@ public class GUIWestminsterShopping {
                         };
 
                         tableModel.addRow(ProductArray);
-
-                    }
-                    else if (selectedCategory.equals("Clothes")){
+                    } else if (selectedCategory.equals("Clothes")) {
                         if (!(product instanceof Clothing)) {
                             continue;
                         }
@@ -163,7 +172,7 @@ public class GUIWestminsterShopping {
                         Category = "Clothes";
                         Information = ((Clothing) product).getProduct_color() + "," + ((Clothing) product).getProduct_size();
 
-                        ProductArray = new Object []{
+                        ProductArray = new Object[]{
                                 product.getProduct_ID(),
                                 product.getProduct_name(),
                                 Category,
@@ -172,24 +181,6 @@ public class GUIWestminsterShopping {
                         };
 
                         tableModel.addRow(ProductArray);
-                    }
-                }
-            }
-        });
-
-        productsTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                if (!e.getValueIsAdjusting()) {
-                    // User has selected a cell
-                    int selectedRow = productsTable.getSelectedRow();
-                    if (selectedRow != -1) {
-                        // Get the product information based on the selected row
-                        productId = productsTable.getValueAt(selectedRow, 0);
-                        productName = productsTable.getValueAt(selectedRow, 1);
-                        category = productsTable.getValueAt(selectedRow, 2);
-                        price = productsTable.getValueAt(selectedRow, 3);
-                        information = productsTable.getValueAt(selectedRow, 4);
                     }
                 }
             }
@@ -200,5 +191,4 @@ public class GUIWestminsterShopping {
         frame.setSize(800, 800);
         frame.setVisible(true);
     }
-
 }
